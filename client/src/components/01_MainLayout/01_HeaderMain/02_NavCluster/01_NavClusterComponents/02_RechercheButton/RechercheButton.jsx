44caf022978f8e88f5efc1f152393f77ff083adb
akliyachir <akli.yachir@gmail.com>
2023-06-04 03:32:25 -0300

    header layout

---
+++ client/src/components/01_MainLayout/01_HeaderMain/02_NavCluster/01_NavClusterComponents/02_RechercheButton/RechercheButton.jsx
@@ -0,0 +1,7 @@
+import "./RechercheButton.css";
+
+export default function RechercheButton() {
+  return (
+    <div className='RechercheButton'>RechercheButton</div>
+  )
+}
--- client/src/components/01_MainLayout/01_HeaderMain/02_NavCluster/NavCluster.jsx
+++ client/src/components/01_MainLayout/01_HeaderMain/02_NavCluster/NavCluster.jsx
@@ -1,10 +1,12 @@
 import HamburgerButton from "./01_NavClusterComponents/01_HamburgerButton/HamburgerButton";
+import RechercheButton from "./01_NavClusterComponents/02_RechercheButton/RechercheButton";
 
 
 export default function NavCluster() {
   return (
     <div>
      <HamburgerButton/>
+     <RechercheButton/>
     </div>
   )
 }
