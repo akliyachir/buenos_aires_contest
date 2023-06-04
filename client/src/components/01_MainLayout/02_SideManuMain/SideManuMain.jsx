ac281faf33730082d6cc630c2b88ad8a1a17e17d
akliyachir <akli.yachir@gmail.com>
2023-06-04 06:31:43 -0300

    header layout

--- client/src/components/01_MainLayout/02_SideManuMain/SideManuMain.jsx
+++ client/src/components/01_MainLayout/02_SideManuMain/SideManuMain.jsx
@@ -1,12 +1,17 @@
 import { useContext } from "react"
 import "./SideManuMain.css"
-import { MenuToggleContext } from "../../../context/MenuContext"
+import { MenuToggleContext } from "../../../context/MenuContext";
+import {AiOutlineCloseCircle} from "react-icons/ai"
 
 
 export default function SideManuMain() {
-const {isMenuOpen} = useContext(MenuToggleContext);
+const {isMenuOpen, setIsMenuOpen} = useContext(MenuToggleContext);
 
- return (
-    <div className={isMenuOpen ? "SideManuMainOpen" : "SideManuMainClosed"}>SideManuMain</div>
+return (
+    <div className={isMenuOpen ? "SideManuMainOpen" : "SideManuMainClosed"}>
+      <div className="closeIconSideMenu" onClick={()=>{setIsMenuOpen(!isMenuOpen)}}>
+     <AiOutlineCloseCircle/>
+      </div>
+    </div>
   )
 }
