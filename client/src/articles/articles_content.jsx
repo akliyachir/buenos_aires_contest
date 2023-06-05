d118c51f66ec6d5fbc2f0f0d8c555331f0ab79bb
akliyachir <akli.yachir@gmail.com>
2023-06-05 05:56:23 -0300

    articles bodies building  lol

--- client/src/articles/articles_content.jsx
+++ client/src/articles/articles_content.jsx
@@ -11,6 +11,67 @@ const articlesContent = [
     art_date: "02 juin 2023",
     art_content: ``,
   },
+  {
+    art_id: "02",
+    art_link_params: "epecuen-village-inonde-escapade-alentours",
+    art_title: "Facturas argentines : noms, types et saveurs",
+    art_title_tag: "BOULANGERIE",
+    art_pic_link:
+      "https://s7e6w6d2.rocketcdn.me/wp-content/uploads/2013/07/factuas.jpg",
+    art_pic_fuente: "Photo: pasteleriaamerica2.com",
+    art_by: "Daniela Gambarotto",
+    art_date: "31 Mai 2023",
+    art_content: ``,
+  },
+  {
+    art_id: "03",
+    art_link_params: "epecuen-village-inonde-escapade-alentours",
+    art_title: "15 bibliothèques de Buenos Aires (dans toutes les langues)",
+    art_title_tag: "LIBRAIRIE",
+    art_pic_link:
+      "https://s7e6w6d2.rocketcdn.me/wp-content/uploads/2014/07/british%20council.jpg",
+    art_pic_fuente: "Ph: Wikipedia Commons",
+    art_by: "Eleonore Riffe",
+    art_date: "29 Mai 2023",
+    art_content: ``,
+  },
+  {
+    art_id: "04",
+    art_link_params: "epecuen-village-inonde-escapade-alentours",
+    art_title: "Les 13 meilleurs bars à jeux à Buenos Aires",
+    art_title_tag: "Hors de Buenos Aires",
+    art_pic_link:
+      "https://s7e6w6d2.rocketcdn.me/wp-content/uploads/2016/04/top-juegos.jpg",
+    art_pic_fuente: "Photo : Facebook Casino Buenos Aires",
+    art_by: "Eva Contreras",
+    art_date: "26 mai 2023",
+    art_content: ``,
+  },
+  {
+    art_id: "05",
+    art_link_params: "epecuen-village-inonde-escapade-alentours",
+    art_title: "Pourboire en Argentine, combien laisse-t-on ?",
+    art_title_tag: "Marie Fages-Brugiroux",
+    art_pic_link:
+      "https://s7e6w6d2.rocketcdn.me/wp-content/uploads/2014/11/Propina-1.jpg",
+    art_pic_fuente: "Ph: Wikipedia Commons",
+    art_by: "Marie Fages-Brugiroux",
+    art_date: "24 Mai 2023",
+    art_content: ``,
+  },
+  {
+    art_id: "06",
+    art_link_params: "epecuen-village-inonde-escapade-alentours",
+    art_title:
+      "6 plats argentins typiques à déguster lors des fêtes nationales",
+    art_title_tag: "Hors de Buenos Aires",
+    art_pic_link:
+      "https://s7e6w6d2.rocketcdn.me/wp-content/uploads/2022/05/120623577_373219547398945_6906231635701054671_n-e1652888678132.jpg",
+    art_pic_fuente: "Ph : Instagram de La Aguada",
+    art_by: "Eva Contreras",
+    art_date: "22 mai 2023",
+    art_content: ``,
+  },
 ];
 
 export default articlesContent;
