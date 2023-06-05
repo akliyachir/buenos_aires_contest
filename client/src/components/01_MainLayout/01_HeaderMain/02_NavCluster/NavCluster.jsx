63fbe7bd97a03e420b9da1445a33540304ff48c8
akliyachir <akli.yachir@gmail.com>
2023-06-05 07:10:56 -0300

    articles bodies building  lol

--- client/src/components/01_MainLayout/01_HeaderMain/02_NavCluster/NavCluster.jsx
+++ client/src/components/01_MainLayout/01_HeaderMain/02_NavCluster/NavCluster.jsx
@@ -3,14 +3,10 @@ import "./NavCluster.css";
 import HamburgerButton from "./01_NavClusterComponents/01_HamburgerButton/HamburgerButton";
 import RechercheButton from "./01_NavClusterComponents/02_RechercheButton/RechercheButton";
 import IdiomaSwitch from "./01_NavClusterComponents/03_IdiomaSwitch/IdiomaSwitch";
-import { useContext } from "react";
-import { WidthPageContext } from "../../../../context/WidthPageContext";
 
 export default function NavCluster() {
-  const { PageWidth } = useContext(WidthPageContext);
   return (
     <div className="NavCluster">
-      <div>{PageWidth}</div>
       <IdiomaSwitch />
       <RechercheButton />
       <HamburgerButton />
