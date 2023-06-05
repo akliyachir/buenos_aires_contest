25508948ec25ae8e954f221f2d567c6d4eda7ead
akliyachir <akli.yachir@gmail.com>
2023-06-05 00:01:27 -0300

    side menu

--- client/src/context/WidthPageContext.jsx
+++ client/src/context/WidthPageContext.jsx
@@ -1,1 +1,13 @@
 import { createContext, useState } from "react";
+
+export const WidthPageContext = createContext(0);
+
+export default function WidthPageContextProvider({children}) {
+ 
+ const [PageWidth, setPageWidth] = useState(0);
+ return(
+  <WidthPageContext.Provider value={{PageWidth, setPageWidth}}>
+{children}
+  </WidthPageContext.Provider>
+ )
+}
