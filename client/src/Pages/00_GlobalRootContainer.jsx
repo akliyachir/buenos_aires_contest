e49de2e9a5b52bdb892e44335a3204d2b0fe55d8
akliyachir <akli.yachir@gmail.com>
2023-06-05 00:21:33 -0300

    side menu

--- client/src/Pages/00_GlobalRootContainer.jsx
+++ client/src/Pages/00_GlobalRootContainer.jsx
@@ -1,16 +1,17 @@
 import RootLayout from './01_RootLayout'
 import SideManuMain from '../components/01_MainLayout/02_SideManuMain/SideManuMain'
 import MenuToggleContextProvider from '../context/MenuContext'
-
+import WidthPageContextProvider from '../context/WidthPageContext'
 
 export default function GlobalRootContainer() {
  return (
-   
+   <WidthPageContextProvider>
    <MenuToggleContextProvider>
     <div className='GlobalRootContainer'>
      <SideManuMain/>
      <RootLayout/>
     </div>
    </MenuToggleContextProvider>
+   </WidthPageContextProvider>
   )
 }
--- client/src/context/WidthPageContext.jsx
+++ client/src/context/WidthPageContext.jsx
@@ -1,10 +1,24 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const WidthPageContext = createContext(0);
 
 export default function WidthPageContextProvider({children}) {
  
  const [PageWidth, setPageWidth] = useState(0);
+ 
+ useEffect(()=> {
+   setPageWidth(globalThis.innerWidth);
+  }, []);
+  
+  useEffect(()=>{
+   globalThis.addEventListener("resize", ()=>{
+   setPageWidth(globalThis.innerWidth);
+  })
+  return globalThis.removeEventListener("resize", ()=>{
+   setPageWidth(globalThis.innerWidth);
+   })
+ })
+
  return(
   <WidthPageContext.Provider value={{PageWidth, setPageWidth}}>
 {children}
