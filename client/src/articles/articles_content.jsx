686b6978abdd71082cc5abe7b2e7415b88a5a348
akliyachir <akli.yachir@gmail.com>
2023-06-04 21:43:20 -0300

    side menu

---
+++ client/src/articles/articles_content.jsx
@@ -0,0 +1,14 @@
+ const articlesContent = [
+ {
+  art_id: 01,
+  art_title: "Epecuen, village fantôme englouti",
+  art_title_tag: "Hors de Buenos Aires",
+  art_pic_link: "https://s7e6w6d2.rocketcdn.me/wp-content/uploads/2015/05/epecuen-696x392.webp",
+  art_pic_fuente: "Ph: Wikipedia Commons",
+  art_by: "Eleonore Riffe",
+  art_date: "02 juin 2023",
+  
+ }
+];
+
+export default articlesContent;
